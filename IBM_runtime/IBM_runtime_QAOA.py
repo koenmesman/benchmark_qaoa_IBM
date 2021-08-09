f999e7c753830f29abe879b4ede0643e4e916873
koenmesman <koenmesman@gmail.com>
2021-08-09 12:49:04 +0200

    Update runtime file

--- IBM_runtime/IBM_runtime_QAOA.py
+++ IBM_runtime/IBM_runtime_QAOA.py
@@ -3,6 +3,11 @@
 from qiskit import transpile, ClassicalRegister, QuantumRegister, QuantumCircuit
 import scipy.optimize as opt
 import numpy as np
+import time
+from numpy import pi
+import datetime
+
+global BENCHMARK_TIMES
 
 def regular_graph(n):
     edges = []
@@ -27,6 +32,16 @@ def eval_cost(out_state, graph):
     for e in edges:
         c += 0.5 * (1 - int(bin_val[e[0]]) * int(bin_val[e[1]]))
     return c
+
+
+def shgo_fun(init_param, graph, p, q_func, backend):  # simplicial homology global optimization
+    bounds = [(0, pi), (0, 2 * pi)]
+    res = []
+    if q_func == 'mcp':
+        min_func = max_cut_norm
+        v, e = graph
+        res = opt.shgo(min_func, bounds, args=(v, e, p, backend),
+                       options={'ftol': 1e-10})  # perhaps v, e can be replaced with graph
 
 
 def prepare_circuits(backend, params, graph, p):
@@ -64,7 +79,7 @@ def prepare_circuits(backend, params, graph, p):
     return transpile(qc, backend)
 
 
-def mcp(params, size, p, iterations, backend):
+def mcp_shgo(params, size, p, iterations, backend):
     """Run and evaluate the QAOA MaxCut problem.
 
     Args:
@@ -80,7 +95,13 @@ def mcp(params, size, p, iterations, backend):
     qc = prepare_circuits(backend, params, graph, p)
     job = backend.run(qc, shots=iterations)
     result = job.result()
-    #time_res = job.time_per_step()
+    print(result)
+    time_res = job.time_per_step()
+
+    times = get_times(time_res)
+    global BENCHMARK_TIMES
+    BENCHMARK_TIMES = merge_times(BENCHMARK_TIMES, times)
+    print(time_res)
     out_state = result.get_counts()
 
     prob = list(out_state.values())
@@ -89,7 +110,34 @@ def mcp(params, size, p, iterations, backend):
     for k in range(len(states)):
         exp += eval_cost(states[k], graph) * prob[k]
 
-    return -exp/iterations
+    return exp/iterations
+
+def merge_times(old_times, new_times):
+    for key, new_val in new_times.items():
+        if old_times[key] and new_times[key]:
+            new_times[key] = new_val + old_times[key]
+    return new_times
+
+def get_times(job):
+    create = job.get('CREATED')-job.get('CREATING')
+    validate = job.get('VALIDATED') - job.get('VALIDATING')
+    if isinstance(job.get('RUNNING'), datetime.datetime) and isinstance(job.get('QUEUED'), datetime.datetime):
+        queue = job.get('RUNNING') - job.get('QUEUED')
+    else:
+        queue = datetime.timedelta()
+
+    if isinstance(job.get('RUNNING'), datetime.datetime):
+        runtime = job.get('COMPLETED') - job.get('RUNNING')
+    else:
+        runtime = datetime.timedelta()
+
+    other = job.get('COMPLETED') - job.get('CREATING') - create - validate - queue - runtime
+    total = job.get('COMPLETED') - job.get('CREATING')
+    out_dict = {'CREATING': create, 'VALIDATING': validate, 'QUEUED': queue, 'RUNNING': runtime, 'OTHER': other, 'CONNECTION': total}
+    for key, val in out_dict.items():
+        out_dict[key] = val.total_seconds()
+
+    return out_dict
 
 def main(backend, user_messenger, **kwargs):
     """Main entry point of the program.
@@ -99,16 +147,29 @@ def main(backend, user_messenger, **kwargs):
         user_messenger: Used to communicate with the program consumer.
         kwargs: User inputs.
     """
+
     iterations = kwargs.pop('iterations', 5)
     p = kwargs.pop('p')
     params = np.zeros(2*p)
     size = 5
-    shots = 10000
+    shots = 1000
+    bounds = [(0, pi), (0, 2 * pi)]
     for it in range(iterations):
+        zero_time = datetime.timedelta()
+        global BENCHMARK_TIMES
+        BENCHMARK_TIMES = {'CREATING': zero_time, 'VALIDATING': zero_time, 'QUEUED': zero_time, 'RUNNING': zero_time,
+                           'OTHER': zero_time, 'CONNECTION': 0, 'WALLTIME': zero_time}
         size += it
-        result = opt.minimize(mcp, params, args=(size, p, shots, backend), method='nelder-mead',
-                       options={'ftol': 1e-2, 'maxfev': 1000, 'disp': False})
+        print(it)
+        #result = opt.minimize(mcp, params, args=(size, p, shots, backend), method='nelder-mead',
+        #               options={'ftol': 1e-2, 'maxfev': 10, 'disp': False})
+        start = time.time()
+        result = opt.shgo(mcp_shgo, bounds, args=(size, p, shots, backend),
+                       options={'ftol': 1e-10, 'maxfev': 1000}) 
+        end = time.time() - start
+        BENCHMARK_TIMES['WALLTIME'] = end
+        print(BENCHMARK_TIMES)
         #result = mcp(params, size, p, backend)
-        user_messenger.publish({"iteration": it, "results": result})
+        user_messenger.publish({"iteration": it, "results": result, "time": end})
 
     user_messenger.publish("All done!", final=True)
