4c1c31b2e728d333149359951281c85ce220980f
koenmesman <koenmesman@gmail.com>
2021-08-09 12:48:32 +0200

    Update main file

--- IBM_runtime/main_QAOA.py
+++ IBM_runtime/main_QAOA.py
@@ -6,13 +6,17 @@ import IBM_runtime_QAOA
 from qiskit import Aer
 from qiskit.providers.ibmq.runtime.utils import RuntimeEncoder, RuntimeDecoder
 from qiskit.providers.ibmq.runtime import UserMessenger
+from qiskit import IBMQ
 
-inputs = {"iterations": 3, "p": 1}
+inputs = {"iterations": 1, "p": 1}
 
-backend = Aer.get_backend('qasm_simulator')
+IBMQ.enable_account('')
+provider = IBMQ.get_provider(hub='strangeworks-hub', group='science-team', project='science-test')
+backend = provider.get_backend("ibm_nairobi") #, account_id="koenmesman")
+
 user_messenger = UserMessenger()
 serialized_inputs = json.dumps(inputs, cls=RuntimeEncoder)
 unserialized_inputs = json.loads(serialized_inputs, cls=RuntimeDecoder)
 
-IBM_runtime.main(backend, user_messenger, **unserialized_inputs)
+IBM_runtime_QAOA.main(backend, user_messenger, **unserialized_inputs)
 
